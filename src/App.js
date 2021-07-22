75c68d3cadf54dc725b6c43cb19551a92b1e7ce6
Iffan Pathan <vbaako1@hotmail.com>
2021-07-22 23:04:50 +0700

    todo before localStorage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
+
+  //! have 2 functions
+  //! F get input to set in state todoinput
+  const handleInputChange = (e) => {
+    //- get input from user update to todo
+    setTodoInput(e.target.value);
+  }
+
+  //! F listen submit tag form 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (todoInput !== "") {
+      //- update data is array to todos
+      setTodos([
+        ...todos,
+        {
+          id: todos.length + 1,
+          text: todoInput.trim()  // trim is "john" <<< from "j o h n"
+        }
+      ])
+    }
+    setTodoInput("");
+  }
+  console.log(todos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          name="todoinput"
+          placeholder="Creact a new todo"
+          value={todoInput}
+          onChange={handleInputChange} // see console.log is  j , jo ,joh , john
+        />
+      </form>
+      {/*  output todo */}
+      <ul className="output-todolist">
+        {/* render todos state */}
+        {todos.map((todo) => (
+          <li key={todoInput.id}>{todo.text} </li>
+        ))}
+      </ul>
     </div>
   );
 }
